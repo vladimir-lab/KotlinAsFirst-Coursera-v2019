5218ce7465bd343ee73aa29a3f70bb3fe600a72d
Azef <litta04@mail.ru>
2019-12-19 17:22:46 +0300

    решены углы и цифра 1го урока

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -2,6 +2,7 @@
 
 package lesson1.task1
 
+import lesson7.task1.markdownToHtml
 import org.junit.Test
 import kotlin.math.*
 
@@ -69,7 +70,7 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours * 3600) + (minutes * 60) + seconds
 
 /**
  * Тривиальная
@@ -78,7 +79,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+    ((sagenes * 48) + (arshins * 16) + vershoks) * 4.445 / 100
 
 /**
  * Тривиальная
@@ -86,7 +88,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (PI * (deg + (min + sec / 60) / 60)) / 180
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (PI * ((deg * 3600) + ((min * 60) + sec)) / (3600 * 180))
 
 /**
  * Тривиальная
@@ -94,7 +96,8 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (PI * (deg + (min + se
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+    sqrt(sqr(abs(x2 - x1)) + sqr(abs(y2 - y1)))
 
 /**
  * Простая
@@ -114,7 +117,8 @@ fun thirdDigit(number: Int): Int {
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+    (((hoursArrive * 60) + minutesArrive) - ((hoursDepart * 60) + minutesDepart))
 
 /**
  * Простая
@@ -123,7 +127,13 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val percentNew: Double = percent.toDouble() / 100
+    val year1: Double = (initial * percentNew)
+    val year2: Double = (initial + year1) * percentNew
+    val year3: Double = (initial + year1 + year2) * percentNew
+    return initial + year1 + year2 + year3
+}
 
 /**
  * Простая
@@ -131,4 +141,10 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    val firstChar: Int = number - ((number / 10) * 10)
+    val secondChar: Int
+    secondChar = (number - firstChar) / 10 - (((number - firstChar) / 100) * 10)
+    val thirdChar: Int = (number - ((secondChar * 10) + firstChar)) / 100
+    return firstChar * 100 + secondChar * 10 + thirdChar
+}
--- test/lesson1/task1/Tests.kt
+++ test/lesson1/task1/Tests.kt
@@ -64,7 +64,7 @@ class Tests {
     @Test
     @Tag("Trivial")
     fun angleInRadian() {
-        assertEquals(0.63256, angleInRadian(36, 14, 39), 1e-5)
+        assertEquals(0.63256, angleInRadian(36, 14, 35), 1e-5)
         assertEquals(PI / 2.0, angleInRadian(90, 0, 0), 1e-5)
     }
 
